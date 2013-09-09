9a2d4f00dcb4dbb98421543ea2f9153e88fcf9bd
Ryan Boland <bolandryanm@gmail.com>
2013-09-09 00:12:58 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timetracker::Application.config.secret_key_base = '0c85dac12fcfbb93a05486422ca8152d9c0424431ba028351e3f3cce524eb047d2e2769bf3cc20915b1a9fb57dacdc2a0f276acfe25f822871d21ba760b54bff'
